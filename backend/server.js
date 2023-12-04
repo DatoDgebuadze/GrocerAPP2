ce408617d7f2607977aa9f7f683c82628bab5834
DatoDgebuadze <davit.dgebuadze.1@btu.edu.ge>
2023-12-04 16:44:19 -0500

    Features implemented for server, inventoryForm updated, GroceriesApp.jsx modified

--- backend/server.js
+++ backend/server.js
@@ -45,3 +45,20 @@ server.get("/products", async (request, response) => {
     const products = await Product.find();
     response.send(products);
 });
+
+
+server.post(".addProduct", async (request, response) =>{
+    const product = request.body
+    const postProduct = new Product({
+        id: product.id,
+        productName: product.productName,
+        brand: product.brand,
+        quantity: product.quantity,
+        image: product.image, 
+        price: product.price
+    });
+
+    const saveProduct = await postProduct.save()
+    saveProduct ? response.send("Product is added to inventory") 
+    : response.send("Failed to add!!!")
+});
--- frontend/src/GroceriesApp.jsx
+++ frontend/src/GroceriesApp.jsx
@@ -3,6 +3,8 @@ import CartList from "./CartList";
 import { products } from "./data/products";
 import { useState, useEffect } from "react";
 import axios from "axios"
+import InventoryForm from "./InventoryForm";
+import product from "../../backend/models/product";
 
 export default function GroceriesApp() {
   //   const [counter, setCounter] = useState(1);
@@ -16,15 +18,47 @@ export default function GroceriesApp() {
 
   const [cartList, setCartList] = useState([]);
   const [products, setProduct] = useState([]);
+  const [formData, setFormData] = useState({
+    id: "",
+    productName: "",
+    brand: "",
+    quantity: "",
+    image: "",
+    price: "",
 
+  });
+
+  const [responseData, setResponseData] = useState(" ");
+
+  // useEffect
   useEffect(() => {
     handleGetProducts();
   }, [])
-
+// Get Products
   const handleGetProducts = async () => {
 
     await axios.get("http://localhost:3000/products").then((response) => { console.log(response.data) })
-  }
+  };
+
+
+// post products
+const handlePostProduct = () => {
+  const postProduct = {
+    id: product.id,
+    productName: product.productName,
+    quantity: product.quantity,
+    image: product.image,
+    price: product.price,
+  };
+
+  axios
+    .post("http://localhost:3000/addProduct", postProduct)
+    .then((response) => {
+      setResponseData(<p>{response.data}</p>);
+      // Handle the response or perform additional actions if needed
+    })
+  
+// adding to cart
   const handleAddToCart = (item) => {
     setCartList((prevList) => {
       console.log(cartList);
@@ -32,6 +66,20 @@ export default function GroceriesApp() {
     });
   };
 
+// Onchange Handler
+const handleOnChange = (evt) => {
+  const fieldName = evt.target.name;
+  const fieldValue = evt.target.value;
+
+  setFormData((prevData) => ({
+    ...prevData,
+    id: crypto.randomUUID(),
+    [fieldName]: fieldValue,
+  }));
+};
+
+
+// emptying cart
   const handleEmptyCart = () => {
     setCartList([]);
   };
@@ -45,6 +93,9 @@ export default function GroceriesApp() {
   return (
     <>
       <h1>Groceries App</h1>
+      <InventoryForm formData = {formData} handleOnChange={handleOnChange} handleOnSubmit = {
+        handlePostProduct
+      }/> 
       <div className="GroceriesApp-Container">
         <InventoryCard list={products} onClick={handleAddToCart} />
         <CartList
