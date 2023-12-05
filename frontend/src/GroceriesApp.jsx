1f8817174804869b2579493dd292531259bf980d
DatoDgebuadze <davit.dgebuadze.1@btu.edu.ge>
2023-12-04 20:30:07 -0500

    latest push

--- frontend/src/GroceriesApp.jsx
+++ frontend/src/GroceriesApp.jsx
@@ -3,20 +3,19 @@ import CartList from "./CartList";
 import InventoryForm from "./InventoryForm";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { v4 as uuidv4 } from 'uuid'; // Import uuid for generating unique IDs
+import { v4 as uuidv4 } from 'uuid';
 
 export default function GroceriesApp() {
   const [cartList, setCartList] = useState([]);
   const [productsList, setProductsList] = useState([]);
   const [formData, setFormData] = useState({
-    id: "",
     productName: "",
     brand: "",
     quantity: "",
     image: "",
     price: "",
   });
-  const [responseData, setResponseData] = useState(""); // Initialize responseData state
+  const [responseData, setResponseData] = useState("");
 
   useEffect(() => {
     handleGetProducts();
@@ -36,9 +35,8 @@ export default function GroceriesApp() {
     try {
       // Submit the form data
       await axios.post("http://localhost:3000/addProduct", formData);
-      setResponseData("Product successfully added!"); // Update responseData on success
+      setResponseData("Product successfully added!");
       setFormData({
-        id: "",
         productName: "",
         brand: "",
         quantity: "",
@@ -48,33 +46,12 @@ export default function GroceriesApp() {
       // Optionally, you can also refresh the product list by calling handleGetProducts();
     } catch (error) {
       console.error("Error posting product:", error);
-      setResponseData("Error adding product. Please try again."); // Update responseData on error
-    }
-  };
-
-
-  const handlePostProduct = async () => {
-    try {
-      const response = await axios.post("http://localhost:3000/addProduct", formData);
-      setProductsList((prevProducts) => [...prevProducts, response.data]);
-    } catch (error) {
-      console.error("Error posting product:", error);
+      setResponseData("Error adding product. Please try again.");
     }
   };
 
   const handleAddToCart = (item) => {
     setCartList((prevList) => [...prevList, { ...item, id: uuidv4() }]);
-  };
-
-  const handleOnChange = (evt) => {
-    const fieldName = evt.target.name;
-    const fieldValue = evt.target.value;
-
-    setFormData((prevData) => ({
-      ...prevData,
-      id: uuidv4(),
-      [fieldName]: fieldValue,
-    }));
   };
 
   const handleEmptyCart = () => {
@@ -88,8 +65,8 @@ export default function GroceriesApp() {
   return (
     <>
       <h1>Groceries App</h1>
-      <InventoryForm formData={formData} handleOnChange={handleOnChange} handleOnSubmit={handleOnSubmit} />
-      <p>{responseData}</p> {/* Render responseData */}
+      <InventoryForm formData={formData} handleOnChange={(e) => setFormData((prevData) => ({ ...prevData, [e.target.name]: e.target.value }))} handleOnSubmit={handleOnSubmit} />
+      <p>{responseData}</p>
       <div className="GroceriesApp-Container">
         <InventoryCard list={productsList} onClick={handleAddToCart} />
         <CartList cartList={cartList} onClickEmpty={handleEmptyCart} onClickRemove={handleRemoveItem} />
--- frontend/src/InventoryForm.jsx
+++ frontend/src/InventoryForm.jsx
@@ -1,40 +1,89 @@
-export default function InventoryForm({formData, handleOnChange, handlePostProduct, handleOnSubmit,}) {
-    return(
-        <div>
-        <form action="" onSubmit={handleOnSubmit}>
-    <div>
-        <label htmlFor="productName">Product Name </label>
-        <input type="text" name="productName" id="productName" onChange={handleOnChange}
-        value={formData.productName}
-        
-        />
-    </div>
-        <div>
-            <label htmlFor="brand"> Brand </label>
-            <input type="text" name="brand" id="brand" onChange={handleOnChange} value={formData.brand}/>
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
+export default function InventoryForm({ formData, handleOnChange, handleOnSubmit }) {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = (data) => {
+        // Handle form submission here
+        console.log(data);
+        handleOnSubmit(data); 
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="productName">Product Name </label>
+                    <input
+                        type="text"
+                        name="productName"
+                        id="productName"
+                        onChange={handleOnChange}
+                        defaultValue={formData.productName}
+                        {...register('productName', { required: 'Product Name is required' })}
+                    />
+                    {errors.productName && <p>{errors.productName.message}</p>}
+                </div>
+
+                <div>
+                    <label htmlFor="brand">Brand </label>
+                    <input
+                        type="text"
+                        name="brand"
+                        id="brand"
+                        onChange={handleOnChange}
+                        defaultValue={formData.brand}
+                        {...register('brand', { required: 'Brand is required' })}
+                    />
+                    {errors.brand && <p>{errors.brand.message}</p>}
+                </div>
+
+                <div>
+                    <label htmlFor="quantity">Quantity </label>
+                    <input
+                        type="text"
+                        name="quantity"
+                        id="quantity"
+                        onChange={handleOnChange}
+                        defaultValue={formData.quantity}
+                        {...register('quantity', { required: 'Quantity is required' })}
+                    />
+                    {errors.quantity && <p>{errors.quantity.message}</p>}
+                </div>
+
+                <div>
+                    <label htmlFor="image">Image URL</label>
+                    <input
+                        type="text"
+                        name="image"
+                        id="image"
+                        onChange={handleOnChange}
+                        defaultValue={formData.image}
+                        {...register('image')}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="price">Price </label>
+                    <input
+                        type="text"
+                        name="price"
+                        id="price"
+                        onChange={handleOnChange}
+                        defaultValue={formData.price}
+                        {...register('price', {
+                            pattern: {
+                                value: /^\d+(\.\d{1,2})?$/,
+                                message: 'Please enter a valid price with up to two decimal places',
+                            },
+                        })}
+                    />
+                    {errors.price && <p>{errors.price.message}</p>}
+                </div>
+
+                <button type="submit">Add to Inventory</button>
+            </form>
         </div>
-        <div>
-            <label htmlFor=" quantity">Quantity </label>
-            <input type="text" name="quantity" id="quantity" onChange={handleOnChange} value={formData.quantity} 
-            />
-
-        </div>
-
-        <div>
-        <label htmlFor="image">Image URL</label>
-        <input type="text" name="image" id="image" onChange={handleOnChange}  value={formData.image}/>
-
-
-        </div>
-
-        <div>
-            <label htmlFor="price"> Price </label>
-            <input type="text" name="price" id="price" onChange={handleOnChange} value={formData.price} />
-        </div>
-        <button>Add to Inventory</button>
-        </form>
-    </div>
-    )
-
+    );
 }
